be1d72e63d15b22e571289c39758f4c0d0bfa7e4
Michael Gallego <gallegom58@gmail.com>
2021-09-10 15:17:28 -0400

    process ffinished successfully with code 0 ffmpeg

--- lib/providers/video_provider.dart
+++ lib/providers/video_provider.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoProvider extends ChangeNotifier {
@@ -16,9 +18,26 @@ class VideoProvider extends ChangeNotifier {
   Future<void> pickVideo() async {
     XFile? pickedFile = await picker.pickVideo(source: ImageSource.gallery);
     _videoFile = File(pickedFile!.path);
+    debugPrint('the path: $_videoFile');
     _videoPlayerController = VideoPlayerController.file(
       _videoFile!,
     )..initialize().then((value) => _videoPlayerController!.play());
     notifyListeners();
+    await extractVideo(_videoFile!.path);
+  }
+
+  Future<void> extractVideo(String input) async {
+    final FlutterFFmpeg _flutterFFmpeg = new FlutterFFmpeg();
+    final output = await getExternalStorageDirectory();
+    // 'ffmpeg -I $input -an output.mp4'
+    await _flutterFFmpeg.executeWithArguments([
+      '-y',
+      '-i',
+      input,
+      '-vcodec',
+      'copy',
+      '-an',
+      '${output!.path}/test.mp4'
+    ]).then((value) => debugPrint('the execution: $value'));
   }
 }
